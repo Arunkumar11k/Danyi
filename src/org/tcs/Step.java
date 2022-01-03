3029a9a5ff74f75cd0c46e38029d8a102b6838b3
Arunkumar11k <k.arunkumar11595@gmail.com>
2022-01-03 13:42:38 +0530

    bshdbv

---
+++ src/org/tcs/Step.java
@@ -0,0 +1,15 @@
+package org.tcs;
+
+public class Step {
+	public void dani() {
+		System.out.println("dani is good boy");
+		
+
+		
+	}
+	public static void main(String[] args) {
+	   Step s = new Step();
+	   s.dani();
+	}
+
+}
